2b508e2357472ffead3b9486c25065ceaae323b1
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-09-06 10:50:19 +0700

    manager_category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,16 @@
 class Category < ApplicationRecord
+  CATEGORY_ATTRS = %w(name description image).freeze
+  mount_uploader :image, PictureUploader
+
   has_many :products, dependent: :destroy
+
+  validates :name, presence: true,
+    length: {maximum: Settings.category.length.max_name}
+  validate  :image_size
+
+  private
+  def image_size
+    return unless image.size > Settings.image.capacity.megabytes
+    errors.add :picture, "should be less than 5MB"
+  end
 end
